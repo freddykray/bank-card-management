df22984af704dd7cfa48f1f3f1f01da0044f74b2
freddykray <rosi.kanal2017@yandex.ru>
2026-04-24 21:10:11 +0300

    Добавил новые класс UserFinder, в целях убрать дублируемый код

---
+++ src/main/java/com/example/bankcards/service/finder/UserFinder.java
@@ -0,0 +1,19 @@
+package com.example.bankcards.service.finder;
+
+import com.example.bankcards.entity.User;
+import com.example.bankcards.exception.NotFoundException;
+import com.example.bankcards.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserFinder {
+
+    private final UserRepository userRepository;
+
+    public User getByIdOrThrow(long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь не найден: id=" + id));
+    }
+}
