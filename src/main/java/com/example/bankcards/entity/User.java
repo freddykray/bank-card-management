47c995003082befbab607e26bdd68088576a4f4d
freddykray <rosi.kanal2017@yandex.ru>
2026-04-24 12:50:13 +0300

    Add User entity

--- src/main/java/com/example/bankcards/entity/User.java
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -1,4 +1,18 @@
 package com.example.bankcards.entity;
 
+import jakarta.persistence.Entity;
+import java.time.Instant;
+
+@Entity
 public class User {
+    private long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+    private UserStatus status;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
 }
