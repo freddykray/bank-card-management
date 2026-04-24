5517f431211dec6282f3386e2dd2900788d72031
freddykray <rosi.kanal2017@yandex.ru>
2026-04-24 12:53:49 +0300

    enums move in folder 'enums'

--- src/main/java/com/example/bankcards/entity/User.java
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -1,18 +1,57 @@
 package com.example.bankcards.entity;
 
+import com.example.bankcards.entity.enums.Role;
+import com.example.bankcards.entity.enums.UserStatus;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
 import java.time.Instant;
 
 @Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
     private String firstName;
+
+    @Column(nullable = false)
+    private String password;
+
     private String lastName;
+
+    @Column(nullable = false)
     private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserStatus status;
+
     private Instant createdAt;
+
     private Instant updatedAt;
+
     private Instant deletedAt;
 }
