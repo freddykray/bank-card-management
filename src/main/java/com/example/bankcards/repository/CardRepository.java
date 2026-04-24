c36831ed1156c425346684e06c75d94dc10ed355
freddykray <rosi.kanal2017@yandex.ru>
2026-04-24 15:49:42 +0300

    Add repositories

---
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -0,0 +1,25 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.Card;
+import com.example.bankcards.entity.enums.CardStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+    List<Card> findAllByDeletedAtIsNull();
+
+    Optional<Card> findByIdAndDeletedAtIsNull(long id);
+
+    List<Card> findAllByUserIdAndDeletedAtIsNull(long userId);
+
+    Optional<Card> findByIdAndUserIdAndDeletedAtIsNull(long id, long userId);
+
+    List<Card> findAllByUserIdAndStatusAndDeletedAtIsNull(long userId, CardStatus status);
+
+    List<Card> findAllByUserIdAndCardNumberLast4AndDeletedAtIsNull(long userId, String cardNumberLast4);
+}
---
+++ src/main/java/com/example/bankcards/repository/TransferRepository.java
@@ -0,0 +1,30 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+
+    @Query("""
+            select t from Transfer t
+            where t.fromCard.user.id = :userId
+               or t.toCard.user.id = :userId
+            """)
+    List<Transfer> findAllByUserId(long userId);
+
+    @Query("""
+            select t from Transfer t
+            where t.id = :transferId
+              and (
+                    t.fromCard.user.id = :userId
+                    or t.toCard.user.id = :userId
+              )
+            """)
+    Optional<Transfer> findByIdAndUserId(long transferId, long userId);
+}
