2585c9614028338fc6073867984eedb2faf1ac8c
freddykray <rosi.kanal2017@yandex.ru>
2026-04-25 18:11:51 +0300

    Реализован метод loadUserByUsername но в нашем случается используется email класс - CustomUserDetailsService

--- src/main/java/com/example/bankcards/security/CustomUserDetailsService.java
+++ src/main/java/com/example/bankcards/security/CustomUserDetailsService.java
@@ -1,4 +1,27 @@
 package com.example.bankcards.security;
 
-public class CustomUserDetailsService {
+import com.example.bankcards.entity.User;
+import com.example.bankcards.exception.NotFoundException;
+import com.example.bankcards.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "Пользователя с таким email не существует"
+                ));
+
+        return new CustomUserDetails(user);
+    }
 }
